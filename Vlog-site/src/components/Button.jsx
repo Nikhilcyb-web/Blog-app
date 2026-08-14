f20dcc3e59cdb888f2bb048fca90e339abf42649
Nikhil <ns45202020@gmail.com>
2026-08-14 16:06:03 +0530

    completed full project

--- Vlog-site/src/components/Button.jsx
+++ Vlog-site/src/components/Button.jsx
@@ -1,15 +1,16 @@
-import React, { Children } from "react";
+import React from "react";
 
-function Button({
-  Children,
-  type='button',
-  bgColor = 'bg-blue-600',
-  className = '',
-  ...props
-})  {
-  return(
-    <button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`}{...props}>
-      {Children}
-    </button>
-  )
+export default function Button({
+    children,
+    type = "button",
+    bgColor = "bg-blue-600",
+    textColor = "text-white",
+    className = "",
+    ...props
+}) {
+    return (
+        <button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`} {...props}>
+            {children}
+        </button>
+    );
 }
--- Vlog-site/src/components/Login.jsx
+++ Vlog-site/src/components/Login.jsx
@@ -1,31 +1,33 @@
-import React, {useState} from "react";
-import {Link , useNavigate} from 'react-router-dom'
-import {Login as authLogin} from '../store/authSlice'
-import {Button, Input, Logo} from './index'
-import { useDispatch } from "react-redux";
-import authService from '../appwrite/auth'
-import {useForm} from 'react-hook-form'
+import React, {useState} from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+import { login as authLogin } from '../store/authSlice'
+import {Button, Input, Logo} from "./index"
+import {useDispatch} from "react-redux"
+import authService from "../appwrite/auth"
+import {useForm} from "react-hook-form"
 
-function Login(){
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const {register, handleSubmit} = useForm()
-  const [error,setError] = useState("")
-  const login = async(data) =>{
-    setError("")
-    try{
-      const session = await authService.login(data)
-      if(session){
-        const userData= await authService.getCurrentUser()
-        if(userData) dispatch(authLogin(userData));
-        navigate("/")
-      }
-    }catch(error){
-      setError(error.message)
+function Login() {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {register, handleSubmit} = useForm()
+    const [error, setError] = useState("")
+
+    const login = async(data) => {
+        setError("")
+        try {
+            const session = await authService.login(data)
+            if (session) {
+                const userData = await authService.getCurrentUser()
+                if(userData) dispatch(authLogin(userData));
+                navigate("/")
+            }
+        } catch (error) {
+            setError(error.message)
+        }
     }
-  }
-  return(
-     <div
+
+  return (
+    <div
     className='flex items-center justify-center w-full'
     >
         <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
@@ -54,8 +56,7 @@ function Login(){
                 {...register("email", {
                     required: true,
                     validate: {
-                        matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.
-                        test(value) ||
+                        matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
                         "Email address must be a valid address",
                     }
                 })}
@@ -78,4 +79,5 @@ function Login(){
     </div>
   )
 }
+
 export default Login
